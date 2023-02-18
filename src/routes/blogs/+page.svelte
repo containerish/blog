87602df76098ba9279766666292fdf211e6b4c94
jay-dee7 <me@jsdp.dev>
2023-02-18 15:05:46 +0530

    fix(deps): Svelte & related dependencies

--- src/routes/blogs/+page.svelte
+++
@@ -1,7 +0,0 @@
-<script>
-    export let data;
-</script>
-
-<div class="h-screen bg-red-300">
-	<span> OpenRegistry Overview </span>
-</div>
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,12 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import type { UserConfig } from 'vite';
 
-export default defineConfig({
-	plugins: [sveltekit()]
-});
+const config: UserConfig = {
+	plugins: [sveltekit()],
+	server: {
+		host: '0.0.0.0',
+		port: 3000
+	}
+};
+
+export default config;
