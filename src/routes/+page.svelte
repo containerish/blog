a1816b7aba8b40f982a6e040f436391f8377d29a
guacamole <gunjanwalecha@gmail.com>
2023-02-28 14:27:45 +0530

    add: Blog on Decentralisation
    - this is an initial draft

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,16 +6,16 @@
 	<title>Blog | Home</title>
 </svelte:head>
 
-<div class="flex gap-3 items-center px-6 my-8 lg:px-20">
+<div class="my-8 flex items-center gap-3 px-6 lg:px-20">
 	<img src="/logo-new.png" alt="logo-openregistr" class="w-12 lg:w-16" />
-	<span class="text-5xl lg:text-7xl font-semibold text-primary-500"> Blog </span>
+	<span class="text-5xl font-semibold text-primary-500 lg:text-7xl"> Blog </span>
 </div>
-<div class="flex flex-col lg:grid lg:grid-cols-6 m-16 gap-10">
+<div class="m-16 flex flex-col gap-10 lg:grid lg:grid-cols-6">
 	<Card styles="col-span-4">
 		<a href="/blogs/openregistry-overview">
 			<img src="/overview-blog.png" alt="collaboration" />
 			<div
-				class="flex justify-center items-center gap-10 text-base lg:text-xl px-2 border-2 py-2 border-stone-400 mx-1 bg-orange-100"
+				class="mx-1 flex items-center justify-center gap-10 border-2 border-stone-400 bg-orange-100 px-2 py-2 text-base lg:text-xl"
 			>
 				<span>5 min read</span>
 				<span>Feb 7 2023</span>
@@ -23,10 +23,9 @@
 		</a>
 	</Card>
 
-	<div class="col-span-2 flex flex-col justify-center gap-5 items-start">
-		<span class="text-lg lg:text-2xl text-primary-600 antialiased"
-			>An Overview on how OpenRegistry works under the hood and in conjuntion with other web3
-			projects.</span
+	<div class="col-span-2 flex flex-col items-start justify-center gap-5">
+		<span class="text-lg text-primary-600 antialiased lg:text-2xl"
+			>An Overview on how OpenRegistry works under the hood and in conjuntion with other web3 projects.</span
 		>
 		<ButtonSolid>learn more</ButtonSolid>
 	</div>
@@ -35,7 +34,7 @@
 		<a href="/blogs/collaborations">
 			<img src="/collaboration-blog.png" alt="collaboration" />
 			<div
-				class="flex justify-center items-center gap-10 text-base lg:text-xl px-2 border-2 py-2 border-stone-400 mx-1 bg-purple-100"
+				class="mx-1 flex items-center justify-center gap-10 border-2 border-stone-400 bg-purple-100 px-2 py-2 text-base lg:text-xl"
 			>
 				<span>5 min read</span>
 				<span>Feb 7 2023</span>
@@ -47,7 +46,7 @@
 		<a href="/blogs/web3-infrastructure">
 			<img src="/blog-web3-infra.png" alt="collaboration" />
 			<div
-				class="flex justify-center items-center gap-10 text-base lg:text-xl px-2 border-2 py-2 border-stone-400 mx-1 bg-green-100"
+				class="mx-1 flex items-center justify-center gap-10 border-2 border-stone-400 bg-green-100 px-2 py-2 text-base lg:text-xl"
 			>
 				<span>5 min read</span>
 				<span>Feb 7 2023</span>
@@ -58,10 +57,21 @@
 		<a href="/blogs/scaling-for-millions">
 			<img src="/scaling/scaling.png/" alt="collaboration" />
 			<div
-				class="flex justify-center items-center gap-10 text-base lg:text-xl px-2 border-2 py-2 border-stone-400 mx-1 bg-blue-100"
+				class="mx-1 flex items-center justify-center gap-10 border-2 border-stone-400 bg-blue-100 px-2 py-2 text-base lg:text-xl"
 			>
 				<span>5 min read</span>
 				<span>Feb 7 2023</span>
+			</div>
+		</a>
+	</Card>
+	<Card styles="col-span-3">
+		<a href="/blogs/decentralisation">
+			<img src="/decentralisation/different-system-arch.png" alt="collaboration" />
+			<div
+				class="mx-1 flex items-center justify-center gap-10 border-2 border-stone-400 bg-blue-100 px-2 py-2 text-base lg:text-xl"
+			>
+				<span>5 min read</span>
+				<span>Feb 28 2023</span>
 			</div>
 		</a>
 	</Card>
